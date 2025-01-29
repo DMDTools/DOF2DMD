a04b131e67e445d46b118b0a63937ef33c3d3672
gustavoalara <gustavoa.laraheredero@gmail.com>
2025-01-29 10:07:23 +0100

    Update demo2.ps1 (fix player variable)

--- DOF2DMD/demo2.ps1
+++ DOF2DMD/demo2.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -URI "http://127.0.0.1:8080/v1/display/picture?path=MatrixCode&duration=30&animation=Fade"
-Invoke-WebRequest -URI "http://127.0.0.1:8080/v1/display/score?players=2&activeplayer=1&score=99345&cleanbg=false"
+Invoke-WebRequest -URI "http://127.0.0.1:8080/v1/display/score?players=2&player=1&score=99345&cleanbg=false"
 Start-Sleep -Seconds 8
-Invoke-WebRequest -URI "http://127.0.0.1:8080/v1/display/score?players=2&activeplayer=2&score=12376&cleanbg=false"
+Invoke-WebRequest -URI "http://127.0.0.1:8080/v1/display/score?players=2&player=2&score=12376&cleanbg=false"
